a35817019a6b5efddeaaf3d25634715ff64c4bd6
zhangweixiao <zwxbest>
2019-02-23 20:21:52 +0800

    modify package name

--- SpringCloudBook-master/eureka-server/src/main/java/com/nizouba/Application.java
+++ SpringCloudBook-master/eureka-server/src/main/java/com/nizouba/Application.java
@@ -1,4 +1,4 @@
-package com.didispace;
+package com.nizouba;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
