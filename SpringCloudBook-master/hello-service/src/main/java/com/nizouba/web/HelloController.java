9d19837d0bdf2daeb1e95ff60f23173b2ded6159
zhangweixiao <zwxbest>
2019-02-23 21:21:08 +0800

    add registry client

---
+++ SpringCloudBook-master/hello-service/src/main/java/com/nizouba/web/HelloController.java
@@ -0,0 +1,25 @@
+package com.nizouba.web;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class HelloController {
+
+	private final Logger logger = Logger.getLogger(getClass());
+
+	@Autowired
+	private DiscoveryClient client;
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String hello() throws Exception {
+		ServiceInstance instance = client.getLocalServiceInstance();
+		logger.info("/hello,host:"+instance.getHost()+",service_id:"+
+			instance.getServiceId());
+		return "Hello World";
+	}
+
+}
