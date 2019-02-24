de4b67789b40a1735cccf3055143befb4574fa9b
zwxbest <zhangweixiao@live.com>
2019-02-24 11:50:13 +0800

    add　docker support

---
+++ SpringCloudBook-master/build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#set -x
+set -eo pipefail
+shopt -s nullglob
+
+echo "$(tput setaf "2")" "repackaging..." "$(tput sgr0)"
+mvn clean package
+for file in ./*-compose.yml; do
+    echo "$(tput setaf "2")" "building $file..." "$(tput sgr0)"
+    docker-compose -f "$file" build
+done
+docker-compose -f docker-compose.yml up -d
+docker rmi $(sudo docker images -f "dangling=true" -q)
