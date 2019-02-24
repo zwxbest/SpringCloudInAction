dc582287b1a800cb74fc3c8c43ca741852083560
zhangweixiao <zhangweixiao@live.com>
2019-02-24 20:26:37 +0800

    add turbine

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 
 ## 分支
 
-本分支对应P187-仪表盘
+本分支对应P192-Turbine集群监控
 
 ## 启动 
 
@@ -22,8 +22,8 @@
 ## 运行
 
 1. 访问http://127.0.0.1:9000/hystrix
-1. 总上面一框中输入http://127.0.0.1:9000/hystrix.stream,一开始会一直loading
+1. 总上面一框中输入http://127.0.0.1:9000/turbine.stream,一开始会一直loading
 1. 多次访问http://127.0.0.1:9000/ribbon-consumer
-1. 观察dashboard
+1. [ ] 一直是loading,这个找原因
 
 
--- ribbon-consumer/src/main/java/com/didispace/ConsumerApplication.java
+++ ribbon-consumer/src/main/java/com/didispace/ConsumerApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableCircuitBreaker
+@EnableTurbine
 @EnableHystrixDashboard
 public class ConsumerApplication {
 
